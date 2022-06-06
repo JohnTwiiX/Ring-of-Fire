f1bac39f3ec5fb3d0f46deec05520fc672ccd3e0
John Fieweger <john.fieweger98@gmail.com>
2022-06-06 18:52:25 +0200

    added models and shuffle cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  pickCardAnimation= false;
+  game = new Game;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
+    
+  }
+
+  newGame(){
+    console.log(this.game);
+  }
+
+  takeCard(){
+    this.pickCardAnimation= true;
   }
 
 }
