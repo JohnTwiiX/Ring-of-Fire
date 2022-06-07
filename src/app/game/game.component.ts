101f0890f10c17f37e81243d92bc80dc3de1a89b
John Fieweger <john.fieweger98@gmail.com>
2022-06-07 19:20:01 +0200

    added player con and dialog con

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
@@ -12,7 +15,8 @@ export class GameComponent implements OnInit {
   game = new Game;
   
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+
 
   ngOnInit(): void {
     this.newGame();
@@ -36,4 +40,12 @@ export class GameComponent implements OnInit {
     }
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name);
+      
+    });
+  }
 }
