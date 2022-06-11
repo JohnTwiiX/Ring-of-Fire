550ae8110dca7544744b9b212816469339c4b101
John Fieweger <john.fieweger98@gmail.com>
2022-06-11 15:59:47 +0200

    added angularfire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-6724b',
+    appId: '1:470129041410:web:033e65be97950fe299471b',
+    storageBucket: 'ring-of-fire-6724b.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBzkAkf72bdIqGZF6pguTPgBaoVX500-C0',
+    authDomain: 'ring-of-fire-6724b.firebaseapp.com',
+    messagingSenderId: '470129041410',
+    measurementId: 'G-9W1X5ZFSTZ',
+  },
   production: false
 };
 
